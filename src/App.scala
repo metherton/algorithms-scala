21015c2a3d5f1518422068beff27cf95c3a627fb
metherton <martinetherton63@gmail.com>
2017-01-17 21:37:03 +0100

    maximum pairwise recursive solution

--- src/App.scala
+++ src/App.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 /**
   * Created by martin on 18/10/16.
   */
@@ -7,8 +9,17 @@ object App {
 
     // maximum pairwise product
     val input = (for {n <- 1 to 2; line = Console.readLine()} yield line).toList
-    val ints = input.last.split(" ").map(_.toLong).sorted
-    println(ints.init.last * ints.last)
+//    val ints = input.last.split(" ").map(_.toLong).sorted
+//    println(ints.init.last * ints.last)
+
+    @tailrec
+    def loop(nums: List[Long], max: Long, nextMax: Long): Long = nums match {
+      case Nil => max * nextMax
+      case x :: xs => if (x > max) loop(xs, x, max) else if (x > nextMax) loop(xs, max, x) else loop(xs, max, nextMax)
+    }
+
+    val product = loop(input.last.split(" ").map(_.toLong).toList, 0, 0)
+    println(product)
   }
 
 }
