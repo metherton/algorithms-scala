ba9c03804bd3058077a777a16ba8aee01cd33735
metherton <martinetherton63@gmail.com>
2017-01-17 22:10:21 +0100

    maximum pairwise recursive solution

--- src/App.scala
+++ src/App.scala
@@ -18,8 +18,22 @@ object App {
       case x :: xs => if (x > max) loop(xs, x, max) else if (x > nextMax) loop(xs, max, x) else loop(xs, max, nextMax)
     }
 
-    val product = loop(input.last.split(" ").map(_.toLong).toList, 0, 0)
-    println(product)
+//    val product = loop(input.last.split(" ").map(_.toLong).toList, 0, 0)
+//    println(product)
+
+    val mpp = new MaximumPairwiseProduct(input.last.split(" ").map(_.toLong).toList)
+    println(mpp.solution())
   }
 
+}
+
+class MaximumPairwiseProduct(val listOfLongIntegers: List[Long]) {
+  def solution(): Long = {
+    @tailrec
+    def solution(longs: List[Long], max: Long, nextMax: Long): Long = longs match {
+      case Nil => max * nextMax
+      case x :: xs => if (x > max) solution(xs, x, max) else if (x > nextMax) solution(xs, max, x) else solution(xs, max, nextMax)
+    }
+    solution(listOfLongIntegers, 0, 0)
+  }
 }
